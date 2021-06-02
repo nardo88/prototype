c2c505375b31e9f52d182355335687c0b88b29a3
nardo88 <nardo1988@mail.ru>
2021-06-02 17:16:34 +0300

    1

--- script.js
+++ script.js
@@ -14,9 +14,48 @@ gsap.timeline({
         end : '600% top',
         scrub: 0.5,
         pin: true,
-        markers: true
+        markers: true,
+
 
     }
 })
 .to('.staps__wrapper', {x: - wrapper.children[0].offsetWidth * 3})
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+ScrollTrigger.create({
+    trigger: '.staps__wrapper',
+    start: 'top top',
+    end : '600% top',
+
+    toggleClass: {targets: '.circle', className: 'circle--active'}
+})
+
+// получили сам круг
+const circle = document.querySelector('.progress-ring__circle');
+const value = document.querySelector('.value');
+// получили радиус круга
+const radius = circle.r.baseVal.value;
+// получаем длинку круга
+const circleLength = 2 * Math.PI * radius;
+
+circle.style.strokeDasharray = `${circleLength} ${circleLength}`;
+circle.style.strokeDashoffset = circleLength;
+
+function progress(persent) {
+    
+    const offSet = circleLength - persent / 100 * circleLength;
+    circle.style.strokeDashoffset = offSet;
+}
+
+
+window.addEventListener('scroll', () => {
+
+    let persent = Math.floor(gsap.getProperty(".staps__wrapper", "x") / (wrapper.children[0].offsetWidth * 3) * 100)
+   
+    value.textContent = `${-persent}`;
+    progress(-persent);
+
+})
 
