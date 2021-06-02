da54b684ac8a9dda9e874785f4b913ff7677db0b
nardo88 <nardo1988@mail.ru>
2021-06-02 16:16:49 +0300

    1

---
+++ script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector('.staps__wrapper');
+const scrollx = [...wrapper.children].reduce((total, item) =>  total += item.offsetWidth, 0);
+
+console.log(scrollx - innerWidth);
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: '.staps__wrapper',
+        start: 'top top',
+        end : '600% top',
+        scrub: true,
+        pin: true,
+        markers: true
+
+    }
+})
+.to('.staps__wrapper', {x: - wrapper.children[0].offsetWidth * 3})
+
